5427b421cf0e8460a38728896b461485c98adc8f
Rodrigo Klaes <rodrigoklaes@gmail.com>
2020-06-23 01:00:48 -0300

    Utilizando pacotes

---
+++ pacotes/reuso/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	goarea "github.com/rodrigoklaes/goarea"
+)
+
+func main() {
+	fmt.Println(goarea.Circ(6.0))
+	fmt.Println(goarea.Rect(3.0, 2.0))
+	// fmt.Println(goarea._TrianguloEq(3.0, 2.0))
+}
